631306c36a8fb8d11a4bb5c35ba0d7de873ed268
Andrew Soltes <soltesandrew@gmail.com>
2023-04-09 02:54:55 +0800

    test

--- dev/main.tf
+++ dev/main.tf
@@ -17,3 +17,15 @@ module "my_ec2_instances" {
   availability_zone   = ""
 
 }
+
+module "atlantis" {
+  source              = "../modules/ec2"
+  ami                 = "ami-01441c79a2c8e534b"
+  instance_count      = 2
+  instance_type       = "t2.micro"
+  instance_name       = "dev-app-ap-se-1"
+  subnet_id           = module.my_network.subnet_id
+  availability_zone   = ""
+
+}
+
